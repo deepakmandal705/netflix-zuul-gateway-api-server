af0fe88ad6af3c11d8f4cc3ceb90c611d62b1bdd
Deepak <deepak.mandal@costrategix.com>
2020-09-21 08:45:33 +0530

    spring cloud API gateway

---
+++ src/main/java/com/deepak/zuulgatewayserver/filter/ZuulLoggingFilter.java
@@ -0,0 +1,44 @@
+package com.deepak.zuulgatewayserver.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext()
+				.getRequest();
+		
+		logger.info("ZuulLoggingFilter Invoked");
+		logger.info("request -> {}", request);
+		logger.info("request -> {}", request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";			//pre,post
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
